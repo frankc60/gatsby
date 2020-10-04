45a1e66b95881c6557ba46846bbb48264bbcf69c
frankc60 <frankc60@gmail.com>
2020-10-04 14:28:28 +0200

    Layout component, header and footer, for all pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
 const blog = () => {
   return (
-    <div>
+    <Layout>
       <h1>this is my blog page</h1>
-      <Link to="/">home page</Link>
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react"
+import Layout from "../components/Layout"
 
 export default class products extends Component {
   render() {
     return (
-      <div>
+      <Layout>
         <h2>my products page!! x</h2>
-      </div>
+      </Layout>
     )
   }
 }
