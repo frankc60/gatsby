ff3530066a85ecc2d8f8d94144c736e17f945bae
frankc60 <frankc60@gmail.com>
2020-10-04 15:27:25 +0200

    module CSS, added to pages, same classname but diff cssModules

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styles from "../components/blog.module.css"
 
 const blog = () => {
   return (
     <Layout>
-      <h1>this is my blog page</h1>
+      <div className={styles.page}>
+        <h1>this is my blog page</h1>
+        <p className={styles.text}>
+          lorem ipsum, blad blahadfsf fsf sdaf sadf saf sdf asfsadf.
+        </p>
+      </div>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
+import styles from "../components/products.module.css"
+console.log(styles)
 
 export default class products extends Component {
   render() {
     return (
       <Layout>
-        <h2>my products page!! x</h2>
+        <div className={styles.page}>
+          <h2>my products page!! x</h2>
+          <p className={styles.text}>hello world from products</p>
+        </div>
       </Layout>
     )
   }
