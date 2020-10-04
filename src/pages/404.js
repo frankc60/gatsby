499bb93482d9826b3b38d7bea59e44ecb9629d33
frankc60 <frankc60@gmail.com>
2020-10-04 14:32:00 +0200

    404.js inlineCSS

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,9 @@ import Layout from "../components/Layout"
 export default function error() {
   return (
     <Layout>
-      <h3>this is our 404 error page</h3>
+      <h3 style={{ color: "red", textTransform: "uppercase" }}>
+        this is our 404 error page
+      </h3>
     </Layout>
   )
 }
