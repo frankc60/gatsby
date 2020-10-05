eb75390061225a05671003cc58d0aba806c6cfeb
frankc60 <frankc60@gmail.com>
2020-10-05 22:22:11 +0200

    add gatsby file source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,16 @@ module.exports = {
     data: ["item1", "item2"],
     person: { name: "pete", age: 43 },
   },
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+
   mapping: {},
 }
