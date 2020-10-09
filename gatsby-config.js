0a72ab494feb5a9454b960f0c27d0eb4c493c977
frankc60 <frankc60@gmail.com>
2020-10-09 23:06:33 +0200

    added gatsby remove-generator plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-remove-generator",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
