7293038df71857b6f2e73d5c9c7bcc0c1042538a
frankc60 <frankc60@gmail.com>
2020-10-25 14:19:12 +0100

    added links to github and contentful on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Link } from "react"
 import Layout from "../components/Layout"
 import { ExampleButton } from "../components/button"
 import styles from "../components/index.module.css"
@@ -9,6 +9,9 @@ export default function Home() {
       <div className={styles.page}>
         <h2>Hello world!!</h2>
         <ExampleButton>click me</ExampleButton>
+        <Link to="https://github.com/frankc60/gatsby">Github page</Link>
+        <Link to="https://app.contentful.com/">Contentful data (products)</Link>
+
         <p className={styles.text}>peace out</p>
       </div>
     </Layout>
